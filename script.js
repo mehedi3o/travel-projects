cc6c4e717ab45702833933eda50657fd788a5584
mehedip1 <Promehedi30@gmail.com>
2024-12-01 11:52:10 +0600

    home sction add

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+let videoBtn = document.querySelectorAll('.vid-btn');
+
 
 Window.onscroll = () =>{
   searchBtn.classList.remove('fa-times');
   searchBar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
+
+
+
+
 }
+
+menu.addEventListener('click', () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+
+})
 
 
 searchBtn.addEventListener('click', () =>{
@@ -26,4 +46,14 @@ formClose.addEventListener('click', () =>{
   loginForm.classList.remove('active');
   
 })
+
+videoBtn.forEach(btn =>{
+  btn.addEventListener('click', () =>{
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+  });
+});
+
 
