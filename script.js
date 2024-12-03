78bc938dac6648d96e4adf2fc8612e9ffeb44209
mehedip1 <Promehedi30@gmail.com>
2024-12-04 00:27:17 +0600

    footer section add

--- script.js
+++ script.js
@@ -109,3 +109,37 @@ var swiper = new Swiper(".review-slider", {
   },
 });
 
+
+var swiper = new Swiper(".brand-slider", {
+
+  spaceBetween: 30,
+  loop:true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+     
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+     
+    },
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+      
+    },
+  },
+});
+
+
+
